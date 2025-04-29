cc15a6b6f9ec83184e965d83ad6aeda849fde768
amulyachinnala <amulya.chinnala07@gmail.com>
2025-04-29 09:07:43 -0400

    Initial Commit

---
+++ Lesson15Output/Lesson15OutputApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson15OutputApp.swift
+//  Lesson15Output
+//
+//  Created by Amulya on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Lesson15OutputApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
